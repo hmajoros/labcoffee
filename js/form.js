67ccdecd9fde95f13c413e18935407baa30095ea
Henry Majoros <majoros.henry@gmail.com>
2015-04-03 16:29:03 -0400

    fixed forms page, started the main look for the home page

--- js/form.js
+++ js/form.js
@@ -5,6 +5,7 @@
         $('input[type="radio"]').on('click', renderRadioSelection);
         
         resizeBG();
+        setDateAndTime();
     });
     
     function resizeBG() {
@@ -45,5 +46,30 @@
             }
         }
     }
+
+    function setDateAndTime() {
+        // var date = new Date().format("m/dd/yy");
+        $('#date').val(setDate);
+        $('#time').val(setTime);
+    }
+
+    function setDate() {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1; // January is 0!
+        var yyyy = today.getFullYear();
+
+        if (dd < 10) dd = '0' + dd; 
+        if (mm < 10) mm = '0' + mm; 
+        today = yyyy + '-' + mm + '-' + dd;
+
+        return today;
+    }
+
+    function setTime() {
+        var today = (new Date()).toTimeString().split(' ')[0];
+        return today.substring(0, 5);
+    }
+
     
 })();
